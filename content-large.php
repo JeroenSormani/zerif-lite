85b9bd5e2557efbeabca6280da77ac947310a560
JeroenSormani <jeroen.sormani@gmail.com>
2016-11-30 19:45:53 +0100

    Code consistency changes for content-large.php

--- content-large.php
+++ content-large.php
@@ -2,17 +2,17 @@
 
 	<?php if ( ! is_search() ) : ?>
 
-		<?php if ( has_post_thumbnail()) : ?>
+		<?php if ( has_post_thumbnail() ) : ?>
 
 		<div class="post-img-wrap-large">
 
 			 	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
 
-					<?php 
-						$image_id = get_post_thumbnail_id();
-						$image_url_big = wp_get_attachment_image_src($image_id,'post-thumbnail-large', true);
-						$image_url_tablet = wp_get_attachment_image_src($image_id,'post-thumbnail-large-table', true);
-						$image_url_mobile = wp_get_attachment_image_src($image_id,'post-thumbnail-large-mobile', true);
+					<?php
+						$image_id         = get_post_thumbnail_id();
+						$image_url_big    = wp_get_attachment_image_src( $image_id, 'post-thumbnail-large', true );
+						$image_url_tablet = wp_get_attachment_image_src( $image_id, 'post-thumbnail-large-table', true );
+						$image_url_mobile = wp_get_attachment_image_src( $image_id, 'post-thumbnail-large-mobile', true );
 					?>
 
 			 		<picture>
@@ -27,13 +27,13 @@
 
 		<div class="listpost-content-wrap-large">
 
-		<?php else: ?>
+		<?php else : ?>
 
 		<div class="listpost-content-wrap-full">
 
 		<?php endif; ?>
 
-	<?php else:  ?>
+	<?php else :  ?>
 
 			<div class="listpost-content-wrap-full">
 
@@ -59,11 +59,11 @@
 
 	<div class="entry-content">
 
-		<?php 
-			$ismore = @strpos( $post->post_content, '<!--more-->');
-			
-			if($ismore) {
-				the_content( sprintf( esc_html__('[...]','zerif-lite'), '<span class="screen-reader-text">'.esc_html__('about ', 'zerif-lite').get_the_title().'</span>' ) );
+		<?php
+			$ismore = @strpos( $post->post_content, '<!--more-->' );
+
+			if ( $ismore ) {
+				the_content( sprintf( esc_html__( '[...]', 'zerif-lite' ), '<span class="screen-reader-text">' . esc_html__( 'about ', 'zerif-lite' ) . get_the_title() . '</span>' ) );
 			} else {
 				the_excerpt();
 			}
@@ -136,14 +136,14 @@
 				<?php endif; /* End if $tags_list */ ?>
 
 			<?php endif; /* End if 'post' == get_post_type() */ ?>
-			
-			
+
+
 			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
 
 			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'zerif-lite' ), __( '1 Comment', 'zerif-lite' ), __( '% Comments', 'zerif-lite' ) ); ?></span>
 
 			<?php endif; ?>
-		
+
 			<?php edit_post_link( __( 'Edit', 'zerif-lite' ), '<span class="edit-link">', '</span>' ); ?>
 
 		</div>
