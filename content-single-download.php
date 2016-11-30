92854061acb2a7a3beb3383ae9e910b9a34aa988
JeroenSormani <jeroen.sormani@gmail.com>
2016-11-30 19:46:01 +0100

    Code consistency changes for content-single-download.php

--- content-single-download.php
+++ content-single-download.php
@@ -13,11 +13,11 @@
 				// check if the post has a Post Thumbnail assigned to it.
 				if ( has_post_thumbnail() ) {
 					the_post_thumbnail();
-				} 
+				}
 			?>
 		</div>
 
-		<?php 
+		<?php
 			the_content();
 		?>
 
