6a7e6d2d467859bb56a41b707ed96d1b603eca91
JeroenSormani <jeroen.sormani@gmail.com>
2016-11-30 19:45:56 +0100

    Code consistency changes for content-none.php

--- content-none.php
+++ content-none.php
@@ -8,8 +8,8 @@
 		<h1 class="page-title"><?php _e( 'Nothing Found', 'zerif-lite' ); ?></h1>
 	</header><!-- .page-header -->
 	<div class="page-content">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>		
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'zerif-lite' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>			
+		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'zerif-lite' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 		<?php elseif ( is_search() ) : ?>
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'zerif-lite' ); ?></p>
 			<?php get_search_form(); ?>
